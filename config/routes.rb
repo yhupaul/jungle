0b3aade840190a7da75b9626fbdb9cc6187d9d8c
yhupaul <yhupaul@hotmail.com>
2021-09-17 01:39:59 -0700

    categories

--- config/routes.rb
+++ config/routes.rb
@@ -12,11 +12,11 @@ Rails.application.routes.draw do
   end
 
   resources :orders, only: [:create, :show]
-  resources :categories, only: [:index, :new, :create]
-
+  
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :new, :create]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
